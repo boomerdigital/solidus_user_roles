6b94cb31d33ba0c7259cb0db02b5df2da919f5e7
Allison C. Reilly <acreilly3@gmail.com>
2016-04-07 12:50:44 -0400

    added specs to assign_permissions

--- spec/models/spree/role_spec.rb
+++ spec/models/spree/role_spec.rb
@@ -4,6 +4,22 @@ describe Spree::Role, type: :model do
   it { should have_many :role_permissions }
   it { should have_many(:permission_sets).through(:role_permissions) }
   it { should validate_uniqueness_of :name }
+  let(:role) {
+    role = build(:role)
+    role.permission_sets << create(:permission_set)
+    role
+  }
+
+  context "#assign_permissions" do
+    it 'creates new Spree::RoleConfiguration::Role' do
+      expect { role.save }.to change { Spree::RoleConfiguration.instance.roles.count }.by(1)
+    end
+    it 'updates the existing Spree::RoleConfiguration::Role' do
+      role.save
+      role.permission_sets << create(:permission_set, name: 'test', set: 'Spree::PermissionSets::ProductDisplay')
+      expect { role.save }.to change { Spree::RoleConfiguration.instance.roles[role.name].permission_sets.count }.from(1).to(2)
+    end
+  end
 
   context "#destroy" do
     before do
